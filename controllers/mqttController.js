d1ad9a76ed66942dd417481db0e0858f796a8474
panamacheng <toptalent.org@outlook.com>
2020-05-19 01:39:20 +0900

    test mode

--- controllers/mqttController.js
+++ controllers/mqttController.js
@@ -6,46 +6,47 @@ const mqttController = (io, mqtt) => {
   // fired when a message is received
   mqtt.on('published', function(packet, client) {
     const data = packet.payload.toString();
-    const topic = payload.topic.toString();
-    if (data) {
-      if (topic == "2677larkindwh") {
-        const newData = {
-          circulation: data.Circulation,
-          supply: data.Supply
-        }
+    // const topic = packet.topic.toString();
+    console.log(data);
+    // if (data) {
+    //   if (topic == "2677larkindwh") {
+    //     const newData = {
+    //       circulation: data.Circulation,
+    //       supply: data.Supply
+    //     }
   
-        Device1Mode2.create(newData)
-          .then(result => {
-            const sendData = {
-              topic: topic, 
-              data: result.get()
-            } 
+    //     Device1Mode2.create(newData)
+    //       .then(result => {
+    //         const sendData = {
+    //           topic: topic, 
+    //           data: result.get()
+    //         } 
 
-            io.emit('event', sendData);
-          })
-          .catch((error) => {
-            console.log(`2677larkindwh storing the data error: ${error}`);
-          });
-      } else {
-        const newData = {
-          indoor: data.Indoor,
-          outdoor: data.Outdoor,
-          return: data.Return
-        }
+    //         io.emit('event', sendData);
+    //       })
+    //       .catch((error) => {
+    //         console.log(`2677larkindwh storing the data error: ${error}`);
+    //       });
+    //   } else {
+    //     const newData = {
+    //       indoor: data.Indoor,
+    //       outdoor: data.Outdoor,
+    //       return: data.Return
+    //     }
   
-        Device1Mode1.create(newData)
-          .then(result => {
-            const sendData = {
-              topic: topic, 
-              data: result.get()
-            } 
-            io.emit('event', sendData);
-          })
-          .catch((error) => {
-            console.log(`2677larkindwh storing the data error: ${error}`);
-          });
-      }
-    }
+    //     Device1Mode1.create(newData)
+    //       .then(result => {
+    //         const sendData = {
+    //           topic: topic, 
+    //           data: result.get()
+    //         } 
+    //         io.emit('event', sendData);
+    //       })
+    //       .catch((error) => {
+    //         console.log(`2677larkindwh storing the data error: ${error}`);
+    //       });
+    //   }
+    // }
   });
 } 
 
